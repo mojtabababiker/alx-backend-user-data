b7ac58b5030284abdc45c6a2d675914ad4b91d91
mojtabababiker <mojmohammad98@gmail.com>
2024-06-03 18:42:43 +0300

    Add extract_base64_authorization_header feature edit coestyle, project 0x01

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,16 +10,15 @@ class BasicAuth(Auth):
     Implementing basic-auth authonitication sechema
     """
     def extract_base64_authorization_header(
-            self, authorization_header: str
-    ) -> str:
-	"""
+            self, authorization_header: str) -> str:
+        """
         Extract the encoded creadential from the authorization_header
         """
-	if not authorization_header:
+        if not authorization_header:
             return None
-	if not isinstance(authorization_header, str):
+        if not isinstance(authorization_header, str):
             return None
-	if not authorization_header.startswith("Basic "):
+        if not authorization_header.startswith("Basic "):
             return None
-	auth = authorization_header[6:]
-	return auth
+        auth = authorization_header[6:]
+        return auth
