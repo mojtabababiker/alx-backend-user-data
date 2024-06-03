a02f941acdafc641c7c2c37ee0fab94319c965a3
mojtabababiker <mojmohammad98@gmail.com>
2024-06-03 18:36:02 +0300

    Edit app.py basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,6 @@
 """
 Basic authorization sechema module holds teh BasicAuth
 """
-from base64 import b64encode, b64decode
-from binascii import Error
-from typing import TypeVar
-from models.user import User
 from api.v1.auth.auth import Auth
 
 
