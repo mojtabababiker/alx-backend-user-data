903216814255dc2692920f501516d1ad4d608c20
mojtabababiker <mojmohammad98@gmail.com>
2024-06-06 12:00:54 +0000

    implement some session creation of the session based auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 Session basef authonetication model
 """
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
@@ -9,3 +10,15 @@ class SessionAuth(Auth):
     """
     Session authorization class
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        create a session id for the current user with id
+        use id, and savee it into session bool
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        user_id_session_id[session_id] = user_id
+        return session_id
