50626ece5324cc2ee03fa38b4c283f1e9d626e95
mojtabababiker <mojmohammad98@gmail.com>
2024-06-06 23:52:34 +0000

    Update the api app, adding new excluded routes and impelement the session cookie option

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,13 +53,15 @@ def before_request():
     """
     pathes = ['/api/v1/status/',
               '/api/v1/unauthorized/',
-              '/api/v1/forbidden/']
+              '/api/v1/forbidden/',
+              '/api/v1/auth_session/login/']
     if auth is None:
         return
     if not auth.require_auth(request.path, pathes):
         return
     if auth.authorization_header(request) is None:
-        abort(401)
+        if auth.session_cookie(request) is None:
+            abort(401)
     if auth.current_user(request) is None:
         abort(403)
     request.current_user = auth.current_user(request)
