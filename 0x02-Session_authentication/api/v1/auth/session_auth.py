0a640c4433860772ef459b8b08c1486b4ef06d16
mojtabababiker <mojmohammad98@gmail.com>
2024-06-06 15:24:19 +0000

    Edit session based class, fix typo, project 0x02

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,5 +20,5 @@ class SessionAuth(Auth):
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid4())
-        self.user_id_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
