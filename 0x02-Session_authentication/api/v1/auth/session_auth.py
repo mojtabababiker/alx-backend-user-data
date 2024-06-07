aee2646a9cfd4f5929c7fb5678ccb365d54f5426
mojtabababiker <mojmohammad98@gmail.com>
2024-06-07 00:03:01 +0000

    Update session based auth class, overloading the current user method, project 0x02

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@ Session basef authonetication model
 """
 from uuid import uuid4
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -31,3 +32,20 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None):
+        """
+        Return a User object based on the cookie value
+        """
+        if request is None:
+            return None
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        # case: no data on the database or no user with user_id
+        try:
+            user = User.get(user_id)
+        except Exception:
+            return None
+        return user
