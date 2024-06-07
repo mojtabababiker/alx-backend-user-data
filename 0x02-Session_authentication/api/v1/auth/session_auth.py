27cba35178cff532d08473b0712d7cf8e2258c12
mojtabababiker <mojmohammad98@gmail.com>
2024-06-07 00:54:22 +0000

    Update session based auth adding logout feature, project 0x02

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """
 Session basef authonetication model
 """
+import os
 from uuid import uuid4
 from api.v1.auth.auth import Auth
 from models.user import User
@@ -23,6 +24,19 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def destroy_session(self, request=None):
+        """
+        Logout user by destorying the session with the id on the request cookie
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        session = self.user_id_for_session_id(session_id)
+        if session is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
