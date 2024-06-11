9314ce0ed19fda109b92fd4b27f027318c51117b
mojtabababiker <mojmohammad98@gmail.com>
2024-06-11 19:38:12 +0300

    Edit user model, make the id auto incremented

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -16,7 +16,7 @@ class User(Base):  # type: ignore
     """
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(255), nullable=False)
     hashed_password = Column(String(255), nullable=False)
     session_id = Column(String(255), nullable=True)
