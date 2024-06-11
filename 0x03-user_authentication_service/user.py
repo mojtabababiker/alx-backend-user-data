ddf574a0880cf11b78c3417d8d04d601a3c943c8
mojtabababiker <mojmohammad98@gmail.com>
2024-06-11 18:50:17 +0300

    Edit user model, meet the requirment for decalaring the table columns, project 0x03

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -16,14 +16,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    reset_token = Column(String, nullable=True)
-
-
-if __name__ == "__main__":
-    print(User.__tablename__)
-
-    for column in User.__table__.columns:
-        print("{}: {}".format(column, column.type))
+    email = Column(String(255), nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    session_id = Column(String(255), nullable=True)
+    reset_token = Column(String(255), nullable=True)
