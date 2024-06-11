543f78c24d5ed41eb982f5b4adec590bd2c695b4
mojtabababiker <mojmohammad98@gmail.com>
2024-06-11 21:09:30 +0300

    Update db module, adding find_user method, project 0x03

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -17,7 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,4 +43,19 @@ class DB:
         self._session.add(user)
         self._session.commit()
 
+        return user
+
+    def find_user_by(self, **kwargs: dict) -> User:
+        """
+        Find and return the first user that match the parameters
+        on the kwargs
+        NoResultFound will be raised when the are no result with those
+        parameters
+        InvalidRequestError will be raised when query (kwargs) contains
+        wrong arguments
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound()
         return user
