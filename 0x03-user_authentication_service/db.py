64a057d3bce5a531cf1a05f3cb3c8b9ad371db65
mojtabababiker <mojmohammad98@gmail.com>
2024-06-11 23:09:27 +0300

    Add all files

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-DB module
+DB module that holds the database engine abstract class
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,20 +13,21 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """DB class, is databse engine abstract class that manage all the
+    databse operations
     """
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """Memoized session object privite property
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -38,7 +39,7 @@ class DB:
         Create a new user with the email and password,
         return the new user object
         """
-        user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password)  # type: ignore
 
         self._session.add(user)
         self._session.commit()
