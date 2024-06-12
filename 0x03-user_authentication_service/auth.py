c0291d5cd866185cec5a1a1b3d12fa6999d872fe
mojtabababiker <mojmohammad98@gmail.com>
2024-06-12 05:39:05 +0200

    Update auth module, add Auth valid_login instance method, project 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,7 @@
 """Auth module that holds the authentication service
 """
 from bcrypt import hashpw, gensalt
+import bcrypt
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
@@ -24,6 +25,7 @@ class Auth:
         """
         Registers a user with the given email and password.
         return the User object.
+        Raise valueError if the email already exists in the database.
         """
         try:
             user = self._db.find_user_by(email=email)
@@ -32,3 +34,13 @@ class Auth:
             pass
         user = self._db.add_user(email, _hash_password(password))
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate the login credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
